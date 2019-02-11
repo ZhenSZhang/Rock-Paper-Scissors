4184057df32e0aacd9a830e2af0e8e8f83b85531
Zhen Zhang <47502758+ZhenSZhang@users.noreply.github.com>
2019-02-11 09:21:19 -0800

    submit V1.0

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Rock Paper Scissors
+
+A simple game I wrote (will keep updating with better code structure)
+
+# What I Learned
+* If/ For Loop {Feb 6 19}
+* Generate Ramdon Number {Feb 6 19}
---
+++ Version 1.0.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                Console.WriteLine("welcome to the game!");
+                Console.WriteLine("type 'rock' 'paper' or 'scissors'");
+                string playerAnswer = Console.ReadLine();
+                Console.WriteLine($"you answered {playerAnswer}");
+                Game game = new Game();
+                String gameAnswer = game.HandGenerator();
+                Console.WriteLine($"here is my answer: {gameAnswer}");
+
+
+                if (gameAnswer == "rock")
+                {
+                    if (playerAnswer == "paper")
+                        Console.WriteLine("you win!");
+                    else if (playerAnswer == "scissors")
+                        Console.WriteLine("you lose!");
+                    else if (playerAnswer == "rock")
+                        Console.WriteLine("we tie!");
+                }
+                else if (gameAnswer == "paper")
+                {
+                    if (playerAnswer == "scissors")
+                        Console.WriteLine("you win!");
+                    else if (playerAnswer == "rock")
+                        Console.WriteLine("you lose!");
+                    else if (playerAnswer == "paper")
+                        Console.WriteLine("we tie!");
+                }
+                else if (gameAnswer == "scissors")
+                {
+                    if (playerAnswer == "rock")
+                        Console.WriteLine("you win!");
+                    else if (playerAnswer == "paper")
+                        Console.WriteLine("you lose!");
+                    else if (playerAnswer == "scissors")
+                        Console.WriteLine("we tie!");
+                }
+
+                Console.WriteLine(" ");
+            }
+        }
+
+    }
+
+
+
+
+    class Game
+    {
+        public int RandomGenerator()
+        {
+            Random randomNumber = new Random();
+            int result = randomNumber.Next(0, 4);
+            return result;
+        }
+
+        public string HandGenerator()
+        {
+            string result;
+            int resultnumber = RandomGenerator();
+            if (resultnumber == 1)
+                return result = "rock";
+            else if (resultnumber == 2)
+                return result = "paper";
+            else
+                return result = "scissors";
+        }
+    }
+       
+}
